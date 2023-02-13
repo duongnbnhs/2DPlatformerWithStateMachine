f0bb23d2e779e85e139e4b33ea060e4c33a2bbd2
duongnb <duongnbse@gmail.com>
2023-02-13 16:42:16 +0700

    + 1LV game play

---
+++ Assets/_Game/Scripts/Character.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] protected Animator anim;
+    [SerializeField] protected HealthBar healthBar;
+
+    [SerializeField] protected CombatText CombatTextPrefab;
+
+
+    private float hp;
+    private string currentAnimName;
+
+    public bool IsDead => hp <= 0;
+
+    private void Start()
+    {
+        OnInit();
+    }
+
+    public virtual void OnInit()
+    {
+        hp = 100;
+        healthBar.OnInit(100, transform);
+    }
+
+    public virtual void OnDespawn()
+    {
+
+    }
+
+    protected virtual void OnDeath()
+    {
+        ChangeAnim(StringHelper.ANIM_DIE);
+        Invoke(nameof(OnDespawn), 2f);
+    }
+
+    protected void ChangeAnim(string animName)
+    {
+        if (currentAnimName != animName)
+        {
+            Debug.Log("new:" + animName);
+            Debug.Log("old:"+currentAnimName);
+            anim.ResetTrigger(animName);
+            currentAnimName = animName;
+            anim.SetTrigger(currentAnimName);
+        }
+    }
+
+    public void OnHit(float damage)
+    {
+        Debug.Log("Hit");
+        if (!IsDead)
+        {
+            hp -= damage;
+
+            if (IsDead)
+            {
+                hp = 0;
+                OnDeath();
+            }
+
+            healthBar.SetNewHp(hp);
+            Instantiate(CombatTextPrefab, transform.position + Vector3.up, Quaternion.identity).OnInit(damage);
+        }
+    }
+
+}
---
+++ Assets/_Game/Scripts/Helper/StringHelper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringHelper : MonoBehaviour
+{
+    public const string ANIM_IDLE = "idle";
+    public const string ANIM_RUN = "run";
+    public const string ANIM_JUMP = "jump";
+    public const string ANIM_ATTACK = "attack";
+    public const string ANIM_THROW = "throw";
+    public const string ANIM_FALL = "fall";
+    public const string ANIM_DIE = "die";
+
+}
