ac3fe44832be60ca258f7febba190d2f5da9e956
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-03-21 15:26:45 +0700

    Add animation magic enemy

--- Assets/_Game/Scripts/Bullets/KunaiMagicPlayer.cs
+++ Assets/_Game/Scripts/Bullets/KunaiMagicPlayer.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class KunaiMagicPlayer : Kunai
 {
-    [SerializeField] private float damageToPlayer;
-    float timer = 0;
+    [SerializeField] private float damageToEnemy;
+    //float timer = 0;
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Enemy")
         {
-            timer += Time.deltaTime;
+            base.timer += Time.deltaTime;
             AudioController.Ins.PlaySound(hitSound);
             collision.GetComponent<Character>().OnHit(30f);
             Instantiate(hitVFX, transform.position, transform.rotation);
