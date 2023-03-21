2792a5dce8ac636ee82ee77ca9f531a02a0c5805
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-03-21 15:50:39 +0700

    Add magicUnlock collect Items

---
+++ Assets/_Game/Scripts/Collection/MagicSkillCollectible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicSkillCollectible : MonoBehaviour
+{
+    [SerializeField] private AudioClip CollectSound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            var player = collision.GetComponent<Player>();
+            player.OnMagicUnlock(true);
+            gameObject.SetActive(false);
+            AudioController.Ins.PlaySound(CollectSound);
+        }
+    }
+}
